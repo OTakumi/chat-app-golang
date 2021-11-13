cb6f383a54e393dd87f7c7b36e876f21cc6a6ad8
OTakumi <takumi.o.0410@gmail.com>
2021-11-13 17:04:34 +0900

    add two functions 1. read message 2. write message

--- internal/client.go
+++ internal/client.go
@@ -12,3 +12,22 @@ type client struct {
 	// The room is chat room which client is joining
 	room *room
 }
+
+func (c *client) read() {
+	for {
+		if _, msg, err := c.socket.ReadMessage(); err == nil {
+			c.room.forward <- msg
+		} else {
+			break
+		}
+	}
+	c.socket.Close()
+}
+
+func (c *client) write() {
+	for msg := range c.send {
+		if err := c.socket.WriteMessage(websocket.TextMessage, msg); err == nil {
+			break
+		}
+	}
+}
