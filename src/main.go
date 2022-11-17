1181a176ef63dba36eca6d100b78b873c750a278
OTakumi <takumi.o.0410@gmail.com>
2022-11-17 11:16:45 +0900

    minor adjustment

--- src/main.go
+++ src/main.go
@@ -26,7 +26,7 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	addr := flag.String("addr", ":8080", "アプリケーションのアドレス")
 	flag.Parse()
 	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
